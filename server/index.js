8ed6b7e27a5e1951555bf5781b3e79a122645866
Grace Casale <gracecasale@gmail.com>
2018-11-17 11:46:06 -0500

    set up forecast route and allowed for router parameteres to pass latitude and longitude similar to darksky api

--- server/index.js
+++ server/index.js
@@ -7,6 +7,17 @@ app.get('/', (request, response) => {
     response.send('Yayyy!!! It works!');
 });
 
+app.get('/forecast/coords/:lat,:lon', (req, res) => {
+    // const lat = req.params.lat; <-- can also be written this way
+    // const lon = req.params.lon;
+    const { lat, lon } = req.params;
+    res.status(200);
+    res.json({
+        lat: lat,
+        lon: lon
+    });
+});
+
 app.listen(port, () => {
     console.log(`Now listening on port: ${port}`);
 })
