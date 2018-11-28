ebb00fbd03458a4f7f8583668d7c1fd376abe9c2
Grace Casale <gracecasale@gmail.com>
2018-11-28 18:20:31 -0500

    linked CurrentWeather.js file to CurrentWeather.css file

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './CurrentWeather.css';
 
 function CurrentWeather(props) {
     const {apparentTemperature} = props.data
     return (
-        <h1>The temperature is currently: {apparentTemperature}&deg;F</h1>
+        <h1 className="current-weather">The temperature is currently: {apparentTemperature}&deg;F</h1>
     )
 }
 
