cd6d3dd28e54583f06feeed97baa225335aa5f1a
Grace Casale <gracecasale@gmail.com>
2018-11-28 19:39:19 -0500

    formatted section for current weather, updated css background color, sizing, and margins.

--- client/src/CurrentWeather.js
+++ client/src/CurrentWeather.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './CurrentWeather.css';
+import cloudyDay from './svg/wi-day-cloudy.svg';
 
 function CurrentWeather(props) {
     const {apparentTemperature} = props.data
     return (
         <section className="cw-container">
         <p className="cw-text">{apparentTemperature}&deg;F</p>
+        <img src={cloudyDay} className="cw-icon"/>
         </section>
     )
 }
