dcc24c4324d7ad927d6cdac307c476e9c074cf20
Grace Casale <gracecasale@gmail.com>
2018-12-03 19:41:21 -0500

    created sections for date, time, and temperature. updated css and adjusted date using date-fns

--- client/src/HourlyWeather.js
+++ client/src/HourlyWeather.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './HourlyWeather.css';
+import format from 'date-fns/format'
 
 const singleHour = hour =>
-    <p key={hour.time} className="hw-container-item">
-        Temperature: {hour.apparentTemperature}&deg;F
-     Time: {new Date(hour.time * 1000).toString()} {/*DarkySky returns time in seconds, JS dates are assumed to be in miliseconds. Multiplying allows these two units to match*/}
-    </p>
+    <div className="hw-bands">
+        <p key={hour.time} className="hw-container-item">
+            Date {"&"} Time: {format(new Date(hour.time * 1000), "MM/DD/YYYY h:mm aa")} {/*DarkySky returns time in seconds, JS dates are assumed to be in miliseconds. Multiplying allows these two units to match*/}
+        </p>
+        <p className="hw-temp">
+            {hour.apparentTemperature}&deg;F
+        </p>
+    </div>
 
 function HourlyWeather(props) {
     const { data } = props.data;
-    const hours = data.filter((hour,index) => new Date(1000 * hour.time).getHours() % 6 === 0).slice(0,4)
+    const hours = data.filter((hour, index) => new Date(1000 * hour.time).getHours() % 6 === 0).slice(0, 4)
     const items = hours
         .map(singleHour)
     return (
